0e03506ce91ba70ab8caa6558ae509710246c470
gundappa patil <patilgundu161@gmail.com>
2023-07-13 11:59:39 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1 className=' text-blue-400'>IMDB project</h1>
+      <Navbar />
     </div>
   );
 }
